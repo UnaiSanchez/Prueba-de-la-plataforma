856688d93946505e37ffef83b4d8c3be9a8f8898
Unai <unai.san.rod@techtalents.club>
2019-07-17 11:17:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+
+require('dotenv').config({path:'./variables.env'})
+
+
+mongoose.connect(process.env.DATABASE)
+
+mongoose.connection.on('error',function(error){
+    console.log('database error:',error)
+})
+
+
+require('./Models/Device')
+
+
+const app = express()
+
+
+app.use(bodyParser())
+app.use(cors())
+
+app.get('/',function(req,res){
+    res.send('funciona')
+})
+
+const routes = require('./Routes/Routes')
+app.use('/',routes)
+
+app.listen(process.env.PORT,function(){
+    console.log('funcionando en el puerto', process.env.PORT)
+})
